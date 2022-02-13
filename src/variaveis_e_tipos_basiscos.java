b663f9156f2d2a0d25db2ba2b516de48b674e6c0
Jean Michael <jeanmmclaudino@gmail.com>
2022-02-12 22:36:40 -0300

    variaveis basicas

---
+++ src/variaveis_e_tipos_basiscos.java
@@ -0,0 +1,13 @@
+public class variaveis_e_tipos_basiscos {
+    public static void main(String[] args){
+        int idade = 25;
+        double altura = 1.90;
+        char sexo = 'M';
+        boolean cool = true;
+        String nome = "Jean Michael";
+
+        System.out.println(nome);
+
+
+    }
+}
