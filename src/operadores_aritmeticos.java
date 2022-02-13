5e42ede848fd6036c54bbb01d549984254984f8a
Jean Michael <jeanmmclaudino@gmail.com>
2022-02-12 22:22:26 -0300

    Operadores Aritmeticos

---
+++ src/operadores_aritmeticos.java
@@ -0,0 +1,12 @@
+public class operadores_aritmeticos {
+    public static void main(String[] args){
+
+        //Operadores Aritméticos
+
+        // + = Adição;
+        // - = Subtração;
+        // * = Multiplicação;  DEVE SER CONSIDERADO ANTES DOS DEMAIS
+        // / = Divisão;        DEVE SER CONSIDERADO ANTES DOS DEMAIS
+        // % = Resto da divisão("mod");
+    }
+}
