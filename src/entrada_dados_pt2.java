87d27063883a100cc9fcb2ea365fc528209bbca6
Jean Michael <jeanmmclaudino@gmail.com>
2022-02-15 20:26:30 -0300

    entrada de dados #2

---
+++ src/entrada_dados_pt2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class entrada_dados_pt2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Como ler um texto até a quebra de linha
+
+        int x;
+        String s1, s2, s3;
+
+        x = sc.nextInt();
+        sc.nextLine();
+        s1 = sc.nextLine();
+        s2 = sc.nextLine();
+        s3 = sc.nextLine();
+
+        System.out.println(x);
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+
+
+        sc.close();
+    }
+}
