27a18c9fc92301f330998f67633480cb3826aaff
Jean Michael <jeanmmclaudino@gmail.com>
2022-02-16 19:56:31 -0300

    expressoes comparativas

---
+++ src/expressoes_comparativas.java
@@ -0,0 +1,19 @@
+public class expressoes_comparativas {
+    public static void main(String[] args) {
+        // Expressões Comparativas
+
+        // < menor que
+        // > maior que
+        // <= menor ou igual
+        // >= maior ou igual
+        // == igual
+        // != diferente
+
+
+        int x = 10;
+        int y = 20;
+
+        System.out.println(x >= y);
+        System.out.println(x <= y);
+    }
+}
