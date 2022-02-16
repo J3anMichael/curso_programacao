93b26e6c440c5b9cbaa734fd6127eb7631d2f18f
Jean Michael <jeanmmclaudino@gmail.com>
2022-02-16 20:08:02 -0300

    expressoes logicas

---
+++ src/expressoes_logicas.java
@@ -0,0 +1,12 @@
+public class expressoes_logicas {
+    public static void main(String[] args) {
+        // && - E
+        // || - OU
+        // ! - NÃO
+
+        int x = 5;
+
+        System.out.println(x <= 20 && x == 10);
+
+    }
+}
