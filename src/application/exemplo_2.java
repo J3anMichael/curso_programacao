c0161de8f0de81c1c0db897f8408f65adaf6daf0
Jean Michael <jeanmmclaudino@gmail.com>
2022-03-07 22:01:19 -0300

    finanlizando exemplo 2

--- src/application/exemplo_2.java
+++ src/application/exemplo_2.java
@@ -16,9 +16,23 @@ public class exemplo_2 {
         System.out.print("Quantity: ");
         product.quantity = sc.nextInt();
 
+        System.out.println();
+        System.out.println("product data: " + product.toString());
 
+        System.out.println("Enter the number of product to be added in stock: ");
+        int quantity = sc.nextInt();
+        product.addProducts(quantity);
 
-        System.out.println(product.toString());
+        System.out.println();
+        System.out.println("Updated data: " + product.toString());
+
+        System.out.println("Enter the number of product to be removed in stock: ");
+        quantity = sc.nextInt();
+        product.removeProducts(quantity);
+
+        System.out.println();
+        System.out.println("Updated data: " + product.toString());
+
 
 
 
